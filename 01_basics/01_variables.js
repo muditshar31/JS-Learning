ec701d6270d864d53986e9e4f2d110c6aeae8e6b
muditshar31 <mudit.shar31@gmail.com>
2026-07-18 18:13:58 +0530

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,22 @@
 
+const accountId = 123456789;
+let accountEmail = "mudit.shar31@gmail.com";
+var accountPassword = "12345";
+
+/*
+Prefer not to use var as it is function scoped and can lead 
+to unexpected behavior. Use let or const instead.
+*/
+
+accountCity = "New York";
+let accountState;
+
+// accountId = 987654321; // accountId is a constant and cannot be reassigned
+
+accountEmail = "test@test.com";
+accountPassword = "54321";
+accountCity = "Los Angeles";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Treat all JS code as newer version, helps avoid errors
+
+alert (3 + 3); // We are using Node.js, so alert will not work here. 
+// It is used in browsers to show popups.
+
+let name = "Mudit";
+let age = 21;
+let IsLoggedIn = false;
+let state; // undefined
+
+/*
+Data types in JS are divided into two categories:
+1. Primitive data types: These are immutable and include:
+- String => "Hello World"
+- Number => 123, 120.50 (2 to power 53 - 1)
+- Boolean => true or false
+- Null => intentional absence of value
+- Undefined => variable declared but not assigned a value
+- BigInt => 1234567890123456789012345678901234567890n (2 to power 53 - 1)
+- Symbol => unique and immutable data type that can be used as an identifier for object properties
+
+2. Non-primitive data types: These are mutable and include:
+- Object
+- Array
+- Function
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object (this is a bug in JS, but it is kept for legacy reasons)
