2122ac252ee53912427b71be7c921897c4a5457d
muditshar31 <mudit.shar31@gmail.com>
2026-07-19 02:24:04 +0530

    Added new file 05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "Mudit";
+const contNumber = 9718004224;
+
+//console.log(`My name is ${name} and my contact number is ${contNumber}.`); 
+// My name is Mudit and my contact number is 9718004224.
+
+console.log(`My name is ${name} and my contact number is ${contNumber}.`); 
+// My name is Mudit and my contact number is 9718004224.
+
+const gameName = new String("Counter Strike");
+
+console.log(gameName[0]); // C
+console.log(gameName.__proto__); // String {0: 'C', 1: 'o', 2: 'u', 3: 'n', 4: 't', 5: 'e', 6: 'r', 7: ' ', 8: 'S', 9: 't', 10: 'r', 11: 'i', 12: 'k', 13: 'e', length: 14, …}
+
+console.log(gameName.length); // 14
+console.log(gameName.toUpperCase()); // COUNTER STRIKE
+console.log(gameName.charAt(8)); // S
+console.log(gameName.indexOf("i")); // 11 => index of first occurrence of "i"
+
+
