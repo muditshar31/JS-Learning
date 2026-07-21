6b340d69f930bae56dfa739b8b157f7c332f3116
muditshar31 <mudit.shar31@gmail.com>
2026-07-21 23:17:39 +0530

    Added few changes in 02_datatypes.js file

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,6 +1,6 @@
 "use strict"; // Treat all JS code as newer version, helps avoid errors
 
-alert (3 + 3); // We are using Node.js, so alert will not work here. 
+// alert (3 + 3); // We are using Node.js, so alert will not work here. 
 // It is used in browsers to show popups.
 
 let name = "Mudit";
@@ -14,7 +14,7 @@ Data types in JS are divided into two categories:
 - String => "Hello World"
 - Number => 123, 120.50 (2 to power 53 - 1)
 - Boolean => true or false
-- Null => intentional absence of value
+- Null => intentional absence of value // null is an object in JS, but it is a primitive data type
 - Undefined => variable declared but not assigned a value
 - BigInt => 1234567890123456789012345678901234567890n (2 to power 53 - 1)
 - Symbol => unique and immutable data type that can be used as an identifier for object properties
@@ -27,3 +27,7 @@ Data types in JS are divided into two categories:
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object (this is a bug in JS, but it is kept for legacy reasons)
+
+// console.log(typeof Object); // object
+// console.log(typeof Array); // object
+// console.log(typeof Function); // function
