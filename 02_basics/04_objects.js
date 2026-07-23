3d39ecdc0f5b10e582ab08c3b5cc85d8a1ba5cb6
muditshar31 <mudit.shar31@gmail.com>
2026-07-24 00:34:15 +0530

    Added examples of Object destructuring & JSON in 04_objects.js file

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,10 +17,10 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname) // { userfullname: { firstname: 'Mudit', lastname: 'Sharma' } }
-console.log(regularUser.fullname.userfullname) // { firstname: 'Mudit', lastname: 'Sharma' }
-console.log(regularUser.fullname.userfullname.firstname) // Mudit
-console.log(regularUser.fullname.userfullname.lastname) // Sharma
+// console.log(regularUser.fullname) // { userfullname: { firstname: 'Mudit', lastname: 'Sharma' } }
+// console.log(regularUser.fullname.userfullname) // { firstname: 'Mudit', lastname: 'Sharma' }
+// console.log(regularUser.fullname.userfullname.firstname) // Mudit
+// console.log(regularUser.fullname.userfullname.lastname) // Sharma
 
 const obj1 = {1 : "a", 2 : "b"}
 const obj2 = {3 : "c", 4 : "d"}
@@ -35,7 +35,7 @@ to a target object */
 
 const obj4 = {...obj1, ...obj2, ...obj3}
 
-console.log(obj4) // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+// console.log(obj4) // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
 
 const users = [
     {
@@ -52,11 +52,47 @@ const users = [
     }
 ]
 
-console.log(users[1].email) // user2@gmail.com
+// console.log(users[1].email) // user2@gmail.com
 
-console.log(tinderUser) // { id: '123abc', name: 'sammy', isLoggedIn: false }
-console.log(Object.keys(tinderUser)) // [ 'id', 'name', 'isLoggedIn' ]
-console.log(Object.values(tinderUser)) // [ '123abc', 'sammy', false ]
-console.log(Object.entries(tinderUser)) // [ [ 'id', '123abc' ], [ 'name', 'sammy' ], [ 'isLoggedIn', false ] ]
+// console.log(tinderUser) // { id: '123abc', name: 'sammy', isLoggedIn: false }
+// console.log(Object.keys(tinderUser)) // [ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser)) // [ '123abc', 'sammy', false ]
+// console.log(Object.entries(tinderUser)) // [ [ 'id', '123abc' ], [ 'name', 'sammy' ], [ 'isLoggedIn', false ] ]
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn')) // true
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')) // true
+
+/* De-Structuring of Object */
+
+const course = {
+    coursename : "JS Basics",
+    price : 999,
+    courseInstructor : "Hitesh"
+}
+
+// console.log(course.courseInstructor)
+
+const {courseInstructor : Instructor, coursename, price} = course
+
+console.log(coursename)
+console.log(price)
+console.log(Instructor)
+
+/* APIs in JSON format in form of Objects */
+
+// {
+//     "name" : "Hitesh",
+//     "courseName" : "JS in Hindi",
+//     "price" : "Free"
+// }
+
+// Sample API : https://api.github.com/users/hiteshchoudhary
+
+/* APIs in form of Arrays */
+
+[
+    {},
+    {},
+    {}
+]
+
+// Sample API : https://randomuser.me/api/
